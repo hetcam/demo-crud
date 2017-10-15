f926a487a1e2ce937db7e4bc5dc4a16780509bfb
Camilo Bermudez <hetcam@gmail.com>
2017-10-15 03:18:47 -0500

    Ajuste para correr proyecto en servidor web

--- src/main/java/com/crud/demo/DemoCrudApplication.java
+++ src/main/java/com/crud/demo/DemoCrudApplication.java
@@ -3,6 +3,8 @@ package com.crud.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
-public class DemoCrudApplication {
+public class DemoCrudApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCrudApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<DemoCrudApplication> applicationClass = DemoCrudApplication.class;
 }
